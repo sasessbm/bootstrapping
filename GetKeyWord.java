c3287e8aca4354f307683b6ee86a672864be87e8
sasessbm <marth.falco.ssbm@gmail.com>
2017-05-24 19:56:15 +0900

    P3とP4で手がかり語獲得機能実装

--- GetKeyWord.java
+++ GetKeyWord.java
@@ -10,17 +10,25 @@ import org.xml.sax.SAXException;
 import makeTriplicity.*;
 
 public class GetKeyWord {
-	
-	private static  ArrayList<Phrase> phraseList;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+		
+		String sentence = "TARGETMEDICINE後、９か月目のMRIの結果・・ 以前壊死を起こしていて、TARGETMEDICINE治療で壊死が消えていた部分一部に、壊死が再発していました";
+		String target = "壊死";
+		String effect = "消えて";
+		String keyWord = "";
+		
+		keyWord = getKeyWord(sentence, target, effect);
+		
+		System.out.println(keyWord);
 
 	}
 	
-	public static ArrayList<String> getKeyWord(String sentence, String target, String effect) throws SAXException, IOException, ParserConfigurationException{
+	public static String getKeyWord(String sentence, String target, String effect) 
+								throws SAXException, IOException, ParserConfigurationException{
 		
-		ArrayList<String> keyWordlist = new ArrayList<String>();
+		String keyWord = "";
+		
 		if(!(sentence.contains(target) && sentence.contains(effect))){ return null; }
 		
 		//構文解析結果をXml形式で取得
@@ -28,28 +36,11 @@ public class GetKeyWord {
 		xmlList = SyntaxAnalys.GetSyntaxAnalysResultXml(sentence);
 
 		//phraseList取得　(phrase,morphemeの生成)
-		//ArrayList<Phrase> phraseList = new ArrayList<Phrase>();
-		phraseList = XmlReader.GetPhraseList(xmlList);
+		ArrayList<Phrase> phraseList = XmlReader.GetPhraseList(xmlList);
+		keyWord = P3SearchKeyWord.keyWord(phraseList, target, effect);
+		//keyWord = P4SearchKeyWord.keyWord(phraseList, target, effect);
 		
-		int targetDependencyIndex = -1;
-		
-		for(Phrase phrase : phraseList){
-			String text = phrase.getPhraseText();
-			if(text.contains(target)){
-				targetDependencyIndex = phrase.getDependencyIndex();
-				
-				continue;
-			}
-		}
-		
-		return keyWordlist;
-		
-	}
-	
-	public static int searchEffectPhrase(int targetDependencyIndex, String effect){
-		
-		
-		
+		return keyWord;
 	}
 	
 	
---
+++ P4SearchKeyWord.java
@@ -0,0 +1,80 @@
+package bootstrapping;
+
+import java.util.ArrayList;
+
+import makeTriplicity.Phrase;
+
+public class P4SearchKeyWord {
+	
+	private static  ArrayList<Phrase> phraseList;
+	public static final String TARGETMEDICINE = "TARGETMEDICINE";
+
+	public static String keyWord(ArrayList<Phrase> phraseList, String target, String effect){
+
+		String keyWord = "";
+		int targetDependencyIndex = -1;
+		int effectId = -1;
+		
+		P4SearchKeyWord.phraseList = phraseList;
+
+		for(Phrase phrase : phraseList){
+			String text = phrase.getPhraseText();
+			if(text.contains(target)){
+				targetDependencyIndex = phrase.getDependencyIndex();
+				effectId = getEffectId(targetDependencyIndex, effect);
+				if(effectId == -1){ continue; }
+				keyWord = getKeyWord(effectId);
+				continue;
+			}
+		}
+		return keyWord;
+	}
+
+	public static int getEffectId(int targetDependencyIndex, String effect){
+
+		int effectId = -1;
+		
+		for(Phrase phrase : phraseList){
+			String text = phrase.getPhraseText();
+			int phraseId = phrase.getId();
+			if(phraseId == targetDependencyIndex && text.contains(effect)){
+				effectId = phraseId;
+				break;
+			}
+		}
+		return effectId;
+	}
+
+	public static String getKeyWord(int effectId){
+
+		int keyWordId = -1;
+		boolean isKeyWordPhrase = false;
+		String keyWord = "";
+
+		for(Phrase phrase : phraseList){
+			int dependencyIndex = phrase.getDependencyIndex();
+			if(dependencyIndex == effectId){
+				keyWordId = phrase.getId();
+				isKeyWordPhrase = judgeKeyWordPhrase(keyWordId);
+				if(isKeyWordPhrase){
+					keyWord = phrase.getMorphemeList().get(0).getMorphemeText();
+					break;
+				}
+			}
+		}
+		return keyWord;
+	}
+	
+	public static boolean judgeKeyWordPhrase(int keyWordId){
+		
+		for(Phrase phrase : phraseList){
+			String text = phrase.getPhraseText();
+			int dependencyIndex = phrase.getDependencyIndex();
+			if(dependencyIndex == keyWordId && text.contains(TARGETMEDICINE)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
